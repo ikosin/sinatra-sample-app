2a1439fee7254de74553e46f3ac79d5c96efa49f
Ikosin <jun62626@gmail.com>
2014-07-20 19:31:08 +0900

    cut out helpers to a separate file

--- app/app.rb
+++ app/app.rb
@@ -7,63 +7,18 @@ require 'digest/sha2'
 require 'tempfile'
 require 'fileutils'
 require 'uuid'
+require File.expand_path '../helpers.rb', __FILE__
 
 class NetPrint < Sinatra::Base
   $UUID    = UUID.new
+
+  helpers Util
 
   configure do
     enable :sessions
   end
   configure :development do
     register Sinatra::Reloader
-  end
-
-  helpers do
-    set :erb, :escape_html => true
-
-    def connection
-      config = JSON.parse(<<EOS
-        {
-          "host": "localhost",
-          "port": 3306,
-          "username": "root",
-          "password": "",
-          "dbname": "netprint"
-        }
-EOS
-      )
-      return $mysql if $mysql
-      $mysql = Mysql2::Client.new(
-        :host => config['host'],
-        :port => config['port'],
-        :username => config['username'],
-        :password => config['password'],
-        :database => config['dbname'],
-        :reconnect => true,
-      )
-    end
-
-    def check_token
-      if params["token"] != session["token"]
-        halt 400, "400 Bad Request"
-      end
-    end
-
-    def hash(str)
-      solt = "Euzd{?.B4huzKTk7m494r#L+Zc4M3(sptg#zieMh46V$n=o8{v"
-      Digest::SHA256.hexdigest(str + solt)
-    end
-
-    def get_hasshed_password(password)
-      $i = 0
-      $num = 5
-
-      while $i < $num  do
-        password = hash(password)
-        $i +=1
-      end
-      password
-    end
   end
 
   get '/' do
